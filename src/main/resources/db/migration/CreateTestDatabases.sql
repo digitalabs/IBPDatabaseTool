7320ac1bd24f5fd332a9a9722ed7a629366eac97
tippsgo <tiffany@efficio.us.com>
2013-09-29 11:49:24 +0800

    init

---
+++ src/main/resources/db/migration/CreateTestDatabases.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS test_central;
+CREATE DATABASE test_central CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+DROP DATABASE IF EXISTS test_local;
+CREATE DATABASE test_local CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+DROP DATABASE IF EXISTS test_groundnut_central;
+CREATE DATABASE test_groundnut_central CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+DROP DATABASE IF EXISTS test_groundnut_local;
+CREATE DATABASE test_groundnut_local CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+DROP DATABASE IF EXISTS test_workbench;
+CREATE DATABASE test_workbench CHARACTER SET utf8 COLLATE utf8_general_ci;
